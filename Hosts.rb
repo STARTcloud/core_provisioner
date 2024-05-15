8d29a705796fdcb9c6c62bc83d635cab77d2df8d
MarkProminic <Mark.Gilbert@prominic.net>
2024-05-15 09:29:13 -0500

    fix: Hosts.rb ansible provisioner galaxy role

--- Hosts.rb
+++ Hosts.rb
@@ -256,8 +256,6 @@ class Hosts
                     ansible.compatibility_mode = localscript['compatibility_mode'].to_s
                     ansible.install_mode = "pip" if localscript['install_mode'] == "pip"
                     ansible.verbose = localscript['verbose']
-                    ansible.galaxy_role_file = "/vagrant/ansible/requirements.yml"
-                    ansible.galaxy_roles_path = "/vagrant/ansible/ansible_collections"
                     ansible.config_file = "/vagrant/ansible/ansible.cfg"
                     ansible.extra_vars = {
                       settings: host['settings'],
@@ -272,7 +270,7 @@ class Hosts
                       ansible_ssh_pipelining:localscript['ssh_pipelining'],
                       ansible_python_interpreter:localscript['ansible_python_interpreter']}
                     if localscript['remote_collections']
-                      ansible.galaxy_role_file = "requirements.yml"
+                      ansible.galaxy_role_file = "/vagrant/ansible/requirements.yml"
                       ansible.galaxy_roles_path = "/vagrant/ansible/ansible_collections"
                     end
                   end
